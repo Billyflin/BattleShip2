cfe80c43e3db985ebd2c3868319b0136d0f24cfc
Billy <billymartinezc@gmail.com>
2022-06-14 00:38:51 -0400

    Parche A16

---
+++ app/src/main/java/com/spm/battleship/ui/game/GameActivity.kt
@@ -0,0 +1,10 @@
+package com.spm.battleship.ui.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/spm/battleship/ui/setup/SetupActivity.kt
@@ -0,0 +1,10 @@
+package com.spm.battleship.ui.setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
