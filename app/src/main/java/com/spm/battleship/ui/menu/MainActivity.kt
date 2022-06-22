c3992c6ccacc7a572567df738ef5f24b45e68a53
Billy <billymartinezc@gmail.com>
2022-06-22 12:15:36 -0400

    rooms2

--- app/src/main/java/com/spm/battleship/ui/menu/MainActivity.kt
+++ app/src/main/java/com/spm/battleship/ui/menu/MainActivity.kt
@@ -2,26 +2,17 @@ package com.spm.battleship.ui.menu
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.spm.battleship.R
 import com.spm.battleship.conf.Init_data.Companion.prefs
-import com.spm.battleship.databinding.ActivityMainBinding
-import com.spm.battleship.models.Resources
 import com.spm.battleship.ui.login.LoginActivity
 import com.spm.battleship.ui.rooms.RoomsActivity
-import com.spm.battleship.ui.rooms.Salas
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        prefs.wipe()
         accessToDetail()
-
     }
 
 
